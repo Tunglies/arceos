7871abe6586d7ebbada72257f89e4cb26ce3036d
Tunglies <tunglies@outlook.com>
2023-12-01 13:24:12 +0800

    step 5

--- apps/loader/src/main.rs
+++ apps/loader/src/main.rs
@@ -35,7 +35,10 @@ fn main() {
     // app running aspace
     // SBI(0x80000000) -> App <- Kernel(0x80200000)
     // 0xffff_ffc0_0000_0000
-    const RUN_START: usize = 0xffff_ffc0_8010_0000;
+    // const RUN_START: usize = 0xffff_ffc0_8010_0000;
+    const RUN_START: usize = 0x4010_0000;
+    unsafe { init_app_page_table(); }
+    unsafe { switch_app_aspace(); }
     let run_code = unsafe {
         core::slice::from_raw_parts_mut(RUN_START as *mut u8, load_size)
     };
@@ -50,6 +53,7 @@ fn main() {
     println!("Execute app ...");
 
     // execute app
+    
     unsafe { core::arch::asm!("
         la      a7, {abi_table}
         li      t2, {run_start}
@@ -58,4 +62,32 @@ fn main() {
         run_start = const RUN_START,
         abi_table = sym ABI_TABLE,
     )}
+}
+
+//
+// App aspace
+//
+
+#[link_section = ".data.app_page_table"]
+static mut APP_PT_SV39: [u64; 512] = [0; 512];
+
+unsafe fn init_app_page_table() {
+    // 0x8000_0000..0xc000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[2] = (0x80000 << 10) | 0xef;
+    // 0xffff_ffc0_8000_0000..0xffff_ffc0_c000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0x102] = (0x80000 << 10) | 0xef;
+
+    // 0x0000_0000..0x4000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[0] = (0x00000 << 10) | 0xef;
+
+    // For App aspace!
+    // 0x4000_0000..0x8000_0000, VRWX_GAD, 1G block
+    APP_PT_SV39[1] = (0x80000 << 10) | 0xef;
+}
+
+unsafe fn switch_app_aspace() {
+    use riscv::register::satp;
+    let page_table_root = APP_PT_SV39.as_ptr() as usize - axconfig::PHYS_VIRT_OFFSET;
+    satp::set(satp::Mode::Sv39, 0, page_table_root >> 12);
+    riscv::asm::sfence_vma_all();
 }
